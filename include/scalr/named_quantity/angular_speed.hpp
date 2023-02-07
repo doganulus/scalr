fafbc0abdbf636f3d7606f33053535e92da7a8e6
Dogan Ulus <dogan.ulus@boun.edu.tr>
2023-02-07 16:40:21 +0000

    Add more angular dimensions

---
+++ include/scalr/named_quantity/angular_speed.hpp
@@ -0,0 +1,41 @@
+/*
+ * Scalr: Physical quantity/unit representation & manipulation library
+ *
+ * Copyright (c) 2023 Dogan Ulus
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#pragma once
+#include <ratio>
+
+#include "scalr/dimension.hpp"
+#include "scalr/quantity.hpp"
+#include "scalr/unit.hpp"
+
+namespace scalr {
+
+struct angular_speed_dimension {
+  using signature = system_signature<-1, -1, 0, 0, 0, 0, 0, 1>;
+};
+
+template <typename Ratio>
+struct angular_speed_unit {
+  using dimension = angular_speed_dimension;
+  using ratio = Ratio;
+};
+
+template <typename Rep, typename Ratio = std::ratio<1>>
+using angular_speed =
+    quantity<Rep, make_unit_t<angular_speed_dimension, Ratio>>;
+
+namespace dimension {
+
+template <>
+struct make<-1, -1, 0, 0, 0, 0, 0, 1> {
+  using type = angular_speed_dimension;
+};
+
+}  // namespace dimension
+}  // namespace scalr
---
+++ include/scalr/named_quantity/moment_of_inertia.hpp
@@ -0,0 +1,41 @@
+/*
+ * Scalr: Physical quantity/unit representation & manipulation library
+ *
+ * Copyright (c) 2023 Dogan Ulus
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#pragma once
+#include <ratio>
+
+#include "scalr/dimension.hpp"
+#include "scalr/quantity.hpp"
+#include "scalr/unit.hpp"
+
+namespace scalr {
+
+struct moment_of_inertia_dimension {
+  using signature = system_signature<0, 2, 1, 0, 0, 0, 0, 0>;
+};
+
+template <typename Ratio>
+struct moment_of_inertia_unit {
+  using dimension = moment_of_inertia_dimension;
+  using ratio = Ratio;
+};
+
+template <typename Rep, typename Ratio = std::ratio<1>>
+using moment_of_inertia =
+    quantity<Rep, make_unit_t<moment_of_inertia_dimension, Ratio>>;
+
+namespace dimension {
+
+template <>
+struct make<0, 2, 1, 0, 0, 0, 0, 0> {
+  using type = moment_of_inertia_dimension;
+};
+
+}  // namespace dimension
+}  // namespace scalr
